dc8c1707d599a597de1c41a1721910bbc4c91a3c
fernsqueen <polinanyan@mail.ru>
2018-04-26 17:08:54 +0500

    IExpression cleaning

---
+++ CALCULATOR/Tests/TestClass.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CALCULATOR;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var nameTable = new Dictionary<string, IExpression>();
+            nameTable["y"] = new FuncExpression("sin", new BinaryOperator("+", new NameExpression("z"), new NameExpression("x")));
+            nameTable["x"] = new FuncExpression("cos", new NameExpression("t"));
+            nameTable["t"] = new ConstantExpression("10");
+
+            IExpression y1 = null;
+            nameTable.TryGetValue("y", out y1);
+            nameTable["y1"] = y1;
+
+            var visitor = new ExpoundVisitor(nameTable);
+
+
+            Console.WriteLine(y1.Accept(visitor).Representation());
+        }
+    }
+}
---
+++ NUnit.Tests1/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using CALCULATOR;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var nameTable = new Dictionary<string, IExpression>();
+            nameTable["y"] = new FuncExpression("sin", new BinaryOperator("+", new NameExpression("z"), new NameExpression("x")));
+            nameTable["x"] = new FuncExpression("cos", new NameExpression("t"));
+            nameTable["t"] = new ConstantExpression("10");
+
+            IExpression y1 = null;
+            nameTable.TryGetValue("y", out y1);
+            nameTable["y1"] = y1;
+
+            var visitor = new ExpoundVisitor(nameTable);
+
+
+            Console.WriteLine(y1.Accept(visitor).Representation());
+        }
+    }
+}
